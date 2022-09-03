1fe91dd6074f2d1537b41897178a762302e26637
demid <sooolomko@mail.ru>
2022-09-03 14:43:48 +0300

    HTML

---
+++ FifthApp/landing_page/crm/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+# статус заявки
+class StatusCrm(models.Model):
+    status_name = models.CharField(max_length=200, verbose_name='Название статуса')
+
+    def __str__(self):
+        return self.status_name
+
+    class Meta():
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+
+# Заказы
+class Order(models.Model):
+    order_dt = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
+    order_name = models.CharField(max_length=200, verbose_name='Имя')
+    order_phone = models.CharField(max_length=50, verbose_name='Телефон')
+    order_status = models.ForeignKey(StatusCrm, on_delete=models.PROTECT, null=True, blank=True, verbose_name='Статус')
+
+    def __str__(self):
+        return self.order_name
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+
+class CommentsCrm(models.Model):
+    comment_binding = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='Заявка')
+    comment_text = models.TextField(verbose_name='Текст комментария')
+    comment_dt = models.DateTimeField(auto_now=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return self.comment_text
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
---
+++ FifthApp/landing_page/price/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import PriceCard
+from .models import PriceTable
+
+admin.site.register(PriceCard)
+admin.site.register(PriceTable)
